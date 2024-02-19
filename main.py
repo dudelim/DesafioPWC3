9c3ed7ee128cb885d5407a662f5ff458a12b761e
dudelim <137728498+dudelim@users.noreply.github.com>
2024-02-19 17:28:52 +0000

    Update

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ def str_endereco(endereco):
   parte_endereco = endereco.split()  # Divide o endereço em partes separadas
   nome_rua = ''
   numero_rua = ''
+
+# Se a primeira parte for um número:
+if parte_endereeco[0].isdigit():
+  numero_rua = partes[0]  # Primeira parte = número
+  nome_rua = ' '.join(partes[1:]) 
