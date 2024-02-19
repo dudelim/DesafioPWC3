ce945b0d69a4a15c74df4f9134dcd385f6507301
dudelim <137728498+dudelim@users.noreply.github.com>
2024-02-19 17:43:45 +0000

    Update

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ def str_endereco(endereco):
         numero_rua, *nome_rua = parte_endereco[0].split(",")  # Divide núm/rua
         nome_rua = ' '.join(nome_rua + parte_endereco[1:])  
     else:
-           # Verifica se há "No"
-           if parte_endereco[-1].lower() == "no":
-               numero_rua = parte_endereco[-1] + " " + parte_endereco[-2] 
-               nome_rua = ' '.join(parte_endereco[:-2])  
-           else:
-               numero_rua = parte_endereco[-1]  # Última parte = número 
-               nome_rua = ' '.join(parte_endereco[:-1])  
+        # Verifica se há "No"
+        if parte_endereco[-1].lower() == "no":
+            numero_rua = parte_endereco[-1] + " " + parte_endereco[-2]  
+            nome_rua = ' '.join(parte_endereco[:-2])  
+        else:
+            numero_rua = parte_endereco[-1]  # Última parte = número
+            nome_rua = ' '.join(parte_endereco[:-1])  
 
-       
+    return nome_rua.strip(), numero_rua.strip()
+
+testes = ["4, Rue de la République", "100 Broadway Av", "Calle Sagasta, 26", "Calle 44 No 1991"]
+
+for teste in testes:
+    rua, numero = str_endereco(teste)
+    print(f"'{teste}' = '{rua}', '{numero}'")
