2489fff14aa1c69963b6bd447dc6d97cc8df9a12
dudelim <137728498+dudelim@users.noreply.github.com>
2024-02-19 17:42:13 +0000

    Update

--- main.py
+++ main.py
@@ -8,4 +8,13 @@ def str_endereco(endereco):
     if "," in parte_endereco[0]:
         numero_rua, *nome_rua = parte_endereco[0].split(",")  # Divide núm/rua
         nome_rua = ' '.join(nome_rua + parte_endereco[1:])  
-   
+    else:
+           # Verifica se há "No"
+           if parte_endereco[-1].lower() == "no":
+               numero_rua = parte_endereco[-1] + " " + parte_endereco[-2] 
+               nome_rua = ' '.join(parte_endereco[:-2])  
+           else:
+               numero_rua = parte_endereco[-1]  # Última parte = número 
+               nome_rua = ' '.join(parte_endereco[:-1])  
+
+       
